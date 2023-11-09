30bdf5a49659549ca70281417635e770f52fbc26
Amadeusz <amadi888@wp.pl>
2023-11-09 20:59:07 +0000

    Firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPwS0jqh9tSIl4cR-dNCjhNA0K7R-PmfU",
+  authDomain: "loss-9cb13.firebaseapp.com",
+  databaseURL: "https://loss-9cb13-default-rtdb.firebaseio.com",
+  projectId: "loss-9cb13",
+  storageBucket: "loss-9cb13.appspot.com",
+  messagingSenderId: "844045767505",
+  appId: "1:844045767505:web:688b72f863e334f465f970",
+};
+
+// Initialize Firebase
+export default app = initializeApp(firebaseConfig);
